b73ebb8abd2692a74b96da08d3c43b65edde53a6
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-11-18 09:04:55 +0200

    Add active states

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Rating.module.scss';
+import { leadingTrim } from 'leading-trim';
 
 const Rating = () => {
   return (
@@ -9,7 +10,15 @@ const Rating = () => {
         Please let us know how we did with your support request. All feedback is appreciated to help
         us improve our offering!
       </p>
-      <button>submit</button>
+      <div className={styles.circles}>
+        <button className={styles.dot}>1</button>
+        <button className={styles.dot}>2</button>
+        <button className={styles.dot}>3</button>
+        <button className={styles.dot}>4</button>
+        <button className={styles.dot}>5</button>
+      </div>
+
+      <button className={styles.button}>SUBMIT</button>
     </section>
   );
 };
