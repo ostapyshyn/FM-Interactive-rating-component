aeed124ab750538d6987713bf3fade7f9a554c78
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-11-19 23:59:59 +0200

    Add star

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Rating.module.scss';
-import { leadingTrim } from 'leading-trim';
+import { ReactComponent as Star } from '../assets/icon-star.svg';
 
 const Rating = () => {
   return (
     <section className={styles.info}>
+      <button className={styles.star}>
+        <Star />
+      </button>
       <h1>How did we do?</h1>
+
       <p>
         Please let us know how we did with your support request. All feedback is appreciated to help
         us improve our offering!
